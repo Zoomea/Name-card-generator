013440b3205623b06e279eb23b2390572a9b1e9b
Michelle Zhou <mi2@student.unimelb.edu.au>
2021-10-08 15:44:06 +0800

    Finish a name card generator app.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./styles.css";
 
-function App() {
+const App = () => {
+  // initialise the contacts as an empty list
+  const [contacts, setContacts] = useState([]);
+
+// give an empty list to useEffect as a second parameter, it will only run once
+// [] as a second parameter in useEffect is called a dependency array
+  useEffect(() => {
+    // React does re-render when the state change, so fetch will be called again and again if not in useEffect(func, []).
+    // fetch three results from the api that generates random people
+    fetch("https://randomuser.me/api/?results=3")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // set the contacts info from the fetch result
+        setContacts(data.results)
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {/* iterate through the contacts to create a contact card for each contact */}
+      {contacts.map(contact => (
+        <CreateContactCard
+          avatar={contact.picture.large}
+          name={contact.name.first + " " + contact.name.last}
+          email={contact.email}
+          age={contact.dob.age}
+        />
+      ))}
+    </>
+  );
+};
+
+
+// mannually create contacts
+// const contacts = [
+//   { name: "Harry Kim", email: "harrykim@kk.com", age: 25 },
+//   { name: "Ed Lam", email: "edlam@kk.com", age: 45 },
+//   { name: "Susan Lim", email: "susanlim@kk.com", age: 33 }
+// ]
+
+//  the function to create display a contact card on the browser
+const CreateContactCard = props => {
+  // console.log(avatar);
+  const [showAge, setShowAge] = useState(false);
+  return (
+    <div className="contact-card">
+      <img src={props.avatar} alt="profile" />
+      <div className="user-details">
+        <p>Name: {props.name}</p>
+        <p>Email: {props.email}</p>
+        <button onClick={() => setShowAge(!showAge)}>
+          Toggle Age
+        </button>
+        {showAge && <p>Age: {props.age}</p>}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
